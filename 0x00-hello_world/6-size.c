8696738d45ecb33167112c6c4a1f2981c80e982b
Bilile <105079809+Sifanedo@users.noreply.github.com>
2022-06-06 01:29:01 -0700

    Redo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int b;
 long long int c;
 char d;
 float f;
-printf("Size of a char: %ld byte(s)\n", (unsigned long)size of(d));
-printf("Size of an int: %ld byte(s)\n", (unsigned long)size of(a));
-printf("Size of a long int: %ld byte(s)\n", (unsigned long)size of(b));
-printf("Size of a long long int: %ld byte(s)\n", (unsigned long)size of(c));
-printf("Size of a float: %ld byte(s)\n", (unsigned long)size of(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(d));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)size of(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)size of(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)size of(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)size of(f));
 return (0);
 }
