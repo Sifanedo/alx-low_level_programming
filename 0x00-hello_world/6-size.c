a8030e10ed78d883f2a384ab259c2428141e8837
Bilile <105079809+Sifanedo@users.noreply.github.com>
2022-06-06 00:03:27 -0700

    Different sizes" EOF

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,15 @@
 */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n");
-printf("Size of an int: 4 byte(s)\n");
-printf("Size of a long int: 8 byte(s)\n");
-printf("Size of a long long int: 8 byte(s)\n");
-printf("Size of a float: 4 byte(s)\n");
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(d));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)size of(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)size of(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)size of(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)size of(f));
 return (0);
 }
