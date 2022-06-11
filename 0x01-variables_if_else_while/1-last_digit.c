57733c790d17d20e63dfb39b129c3ecea9c33a0e
Bilile <105079809+Sifanedo@users.noreply.github.com>
2022-06-11 01:12:53 -0700

    Write a program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,13 +7,15 @@
 *number stored in the variable n
 *Return: Always 0 (Success)
 */
-
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+{
 printf("last digit of %d is %d", n, n % 10);
+}
 if (n % 10 > 5)
 {
 printf(" and is greater than 5\n");
@@ -22,7 +24,7 @@ else if (n % 10 == 0)
 {
 printf(" and is 0\n");
 }
-else
+else if (n % 10 < 6 && n % 10 != 0)
 {
 printf(" and is less than 6 and not 0\n");
 }
