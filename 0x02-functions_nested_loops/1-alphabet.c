58b597faffaf7053a4047f135505c16c6126c702
Bilile <105079809+Sifanedo@users.noreply.github.com>
2022-06-13 14:33:40 -0700

    Remove implicit declaration

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*print_alphabet - prints the alphabet,
+*main - prints the alphabet,
 *in lowercase,
 *followed by a new line
 *Return: Always 0 (Success)
 */
-void print_alphabet(void)
+void main(void)
 {
 char ch;
-{
+
 for (ch = 'a'; ch <= 'z'; ch++)
+{
 putchar(ch);
 }
 {
