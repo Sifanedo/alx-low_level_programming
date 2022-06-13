b280e6c97d1766cb07515b5f292812f53f2c8f7b
Bilile <105079809+Sifanedo@users.noreply.github.com>
2022-06-13 06:28:36 -0700

    main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,13 @@
 *followed by a new line
 *Return: Always 0 (Success)
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char ch;
  
 for (ch = 'a'; ch <= 'z'; ch++)
-
 {
 putchar(ch);
 }
 putchar('\n');
-return (0);
 }
